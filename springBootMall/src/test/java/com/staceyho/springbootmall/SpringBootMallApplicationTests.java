110a2eb90225fed88ba5662499ff4a88e6f73270
hakusaiHo <a88666089@gmail.com>
2023-05-21 12:59:16 +0800

    初始化springBootProject

---
+++ springBootMall/src/test/java/com/staceyho/springbootmall/SpringBootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staceyho.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
