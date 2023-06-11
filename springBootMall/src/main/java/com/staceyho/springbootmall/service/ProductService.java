ac7263a12610e2916edef2cf14c081c61d3479a8
hakusaiHo <a88666089@gmail.com>
2023-06-11 15:59:35 +0800

    update 商品功能/查詢商品

--- springBootMall/src/main/java/com/staceyho/springbootmall/service/ProductService.java
+++ springBootMall/src/main/java/com/staceyho/springbootmall/service/ProductService.java
@@ -1,4 +1,9 @@
 package com.staceyho.springbootmall.service;
 
+import com.staceyho.springbootmall.model.Product;
+
 public interface ProductService {
+
+    Product getProductByID(Integer productId);
+
 }
--- springBootMall/src/main/java/com/staceyho/springbootmall/service/impl/ProductServiceImpl.java
+++ springBootMall/src/main/java/com/staceyho/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,8 +1,19 @@
 package com.staceyho.springbootmall.service.impl;
 
+import com.staceyho.springbootmall.dao.ProductDao;
+import com.staceyho.springbootmall.model.Product;
 import com.staceyho.springbootmall.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Override
+    public Product getProductByID(Integer productId) {
+        return productDao.getProductById(productId);
+    }
 }
